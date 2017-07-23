139dffb1b9720e984f039a632c6cbf94b0754321
Luwei Zhang <luzhang@linkedin.com>
2017-07-22 20:57:29 -0700

    add utils and eliminate() function

--- aind-sudoku/solution.py
+++ aind-sudoku/solution.py
@@ -1,3 +1,5 @@
+from utils import *
+
 assignments = []
 
 def assign_value(values, box, value):
@@ -59,7 +61,23 @@ def display(values):
     return
 
 def eliminate(values):
-    pass
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
+
 
 def only_choice(values):
     pass
